9c32b45595f143d532f64cdc0b9c8eccd3e07267
Joe Kutner <jpkutner@gmail.com>
2016-01-03 23:01:29 -0600

    Updated release script

--- release.sh
+++ release.sh
@@ -9,31 +9,31 @@ mkdir -p target
 cd mruby/build/
 
 cd i386-apple-darwin14/bin
-zip mjava_macosx_bin.zip mjava
+zip mjava_i686-apple-darwin.zip mjava
 mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-apple-darwin14/bin
-zip mjava_macosx_x86_64_bin.zip mjava
+zip mjava_x86_64-apple-darwin.zip mjava
 mv *.zip ../../../../target
 cd ../..
 
 cd i686-pc-linux-gnu/bin
-zip mjava_linux_bin.zip mjava
+zip mjava_i686-pc-linux-gnu.zip mjava
 mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-pc-linux-gnu/bin
-zip mjava_linux_x86_64_bin.zip mjava
+zip mjava_x86_64-pc-linux-gnu.zip mjava
 mv *.zip ../../../../target
 cd ../..
 
 cd i686-w64-mingw32/bin
-zip mjava_win_exe.zip mjava.exe
+zip mjava_windows_intelx86.zip mjava.exe
 mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-w64-mingw32/bin
-zip mjava_win_x86_64_exe.zip mjava.exe
+zip mjava_windows_x86_64.zip mjava.exe
 mv *.zip ../../../../target
 cd ../..
