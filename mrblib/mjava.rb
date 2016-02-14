286ad63eba79686638be115701e7214080871616
Joe Kutner <jpkutner@gmail.com>
2016-02-14 10:40:50 -0600

    Removed unneeded cleanup

--- mrblib/mjava.rb
+++ mrblib/mjava.rb
@@ -5,6 +5,7 @@ end
 def __main__(argv)
   command = argv.shift
 
+  mjava_in_proc = false
   mjava_home = nil
   mjava_zip = nil
   mjava_unzip_dir = ".jdk"
@@ -18,6 +19,9 @@ def __main__(argv)
     elsif arg.start_with?("--mjava-unzip-dir=")
       mjava_unzip_dir = arg["--mjava-unzip-dir=".size..-1]
       true
+    elsif arg == "--mjava-in-proc"
+      mjava_in_proc = true
+      true
     else
       false
     end
@@ -28,18 +32,28 @@ def __main__(argv)
     Miniz.unzip(mjava_zip, mjava_unzip_dir)
 
     mjava_home = "#{mjava_unzip_dir}/#{mjava_home}"
-    mjava_bin_dir = "#{mjava_home}/bin"
-    Dir.foreach(mjava_bin_dir) do |f|
-      File.chmod2("0755", "#{mjava_bin_dir}/#{f}") unless f.start_with?(".")
-    end
   end
 
   debug "mjava_home = #{mjava_home}"
   debug "mjava_zip  = #{mjava_zip}"
 
-  if mjava_home
-    JavaSupport.custom_java(mjava_home, argv)
+  java_support = mjava_home ? JavaSupport.new(mjava_home) : JavaSupport.new
+
+  if mjava_in_proc
+    java_opts = []
+    while argv.first != "--" and !argv.empty?
+      java_opts << argv.shift
+    end
+    argv.shift
+    java_class = argv.shift
+    java_support.exec_java(java_opts, java_class, argv)
   else
-    JavaSupport.system_java(argv)
+    if mjava_zip
+      mjava_bin_dir = "#{mjava_home}/bin"
+      Dir.foreach(mjava_bin_dir) do |f|
+        File.chmod2("0755", "#{mjava_bin_dir}/#{f}") unless f.start_with?(".")
+      end
+    end
+    java_support.system_java(argv)
   end
 end
